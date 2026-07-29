2a561e0815e91d73c9f890aeee88c3842e871278
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-07-29 22:36:24 +0530

    Changes in c repo from mac

--- Sheet_1.c
+++ Sheet_1.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
-
+// /* Question 1 --> Find the largest of three */
+int largestOfThree(int,int,int);
 int main(){
-    printf("This is c \n");
+    int a,b,c;
+    a = 8;
+    b = 20;
+    c = 100;
+    int d = largestOfThree(a,b,c);
+    printf("%d \n",d);
+    return 0;
+}
+
+
+// /* Question 1 --> Find the largest of three */
+int largestOfThree(int a,int b,int c)  {
+    if(a > b){
+        if(a > c){
+            return a;
+        }
+        else{
+            return c;
+        }
+    }else{
+        if(b > c){
+            return b;
+        }else{
+            return c;
+        }
+    }
     return 0;
 }
