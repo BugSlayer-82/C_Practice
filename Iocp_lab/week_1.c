0ff12676215f1fd9e679fcd08997bb776b9053a2
Popoye-75 <kasifmohd818@gmail.com>
2026-08-02 10:09:25 +0530

    some question solved from linux

--- Iocp_lab/week_1.c
+++ Iocp_lab/week_1.c
@@ -5,22 +5,36 @@
 
 
 // /* Question 1 --> Count the total digit of number (Optimised approach) */ 
-int countOfDigit(int num){
-    if(num == 0){
-        return 1;
-    }
-    return (int)log10(num) + 1;
-}
+// int countOfDigit(int num){
+//     if(num == 0){
+//         return 1;
+//     }
+//     int count = 0;
+//     while(num != 0){
+//         num /= 10;
+//         count ++;
+//     }
+//     return count;
+// }
+
+// int countOfDigit(int num){
+//     if(num == 0){ // If number is 0 so return 1
+//         return 1;
+//     }
+//     return (int)log10(num) + 1;// Count the number of digit
+// }
+
 int main()
 {
     // /* Question --> 1 */
     int num;  // To declare a varible that store number
     printf("Enter your number : "); 
     scanf("%d",&num); // Take input from user
-    printf("Count : %d \n",countOfDigit(num)); // Print and call the count Digit function 
+    // printf("Count : %d \n",countOfDigit(num)); // Print and call the count Digit function 
+    printf("Count : %d \n",countOfDigit(num));
+
 
     // * Question --> 2 */
-    int num;
 
     return 0;
 }
