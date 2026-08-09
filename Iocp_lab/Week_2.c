e9984628d90c7a83b00e58f49e254243f850bb9f
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-08-09 18:04:34 +0530

    updates from win

--- Iocp_lab/Week_2.c
+++ Iocp_lab/Week_2.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+void rightAngle_3(int n); // Right angle triangle with character
+void rightAngle_2(int n); // Right angle triangle with number 
+void rightAngle_1(int n); // Right angle triangle with star
+
 int main(){
     
     return 0;
