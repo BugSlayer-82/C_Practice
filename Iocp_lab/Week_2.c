dd5edc88ff3764ec2c5ce56374bcd2d230c1886a
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-08-09 18:25:36 +0530

    Changes from win

--- Iocp_lab/Week_2.c
+++ Iocp_lab/Week_2.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 
-void rightAngle_3(int n); // Right angle triangle with character
+void rightAngle_3(char ch); // Right angle triangle with character
 void rightAngle_2(int n); // Right angle triangle with number 
 void rightAngle_1(int n); // Right angle triangle with star
 
 int main(){
-    
+    int n;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    // rightAngle_1(n);
+    // rightAngle_2(n);
     return 0;
 }
+
+// Question 3 --> Right angle triangle with character
+void rightAngle_3(char ch){
+    for()
+}
+
+// Question 2 --> Right angle triangle with number
+// void rightAngle_2(int n){
+//     for(int i = 1; i <= n; i++){
+//         for(int j = 1; j <= i; j++){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// Question 1 --> Right angle triangle with star 
+// void rightAngle_1(int n){
+//     for(int i = 1; i <= n ; i++){
+//         for(int j = 1; j <= i; j++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
