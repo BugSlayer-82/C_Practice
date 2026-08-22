efb992f6613af280b8d7b6f11213a7ff5589efcf
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-08-22 09:03:36 +0530

    two Question solved

--- Iocp_lab/Week_4.c
+++ Iocp_lab/Week_4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <limits.h>
 
+// /* Question 2 ===> Find Second largest element in array */
 int secondLargest(int arr[],int length)
 {
     int max = INT_MIN;
@@ -21,10 +22,24 @@ int secondLargest(int arr[],int length)
 
     return secMax;
 }
+
+// /* Question 1 ===> Find largest element in array */ 
+int largest(int arr[],int length){
+    int max =  INT_MIN; 
+    for(int i = 0; i < length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
 int main()
 {
     int arr[] = {8,8,8,9,8};
     int length = sizeof(arr) / sizeof(arr[0]);
+    printf("Largest : %d \n",largest(arr,length));
     printf("Second largest : %d\n",secondLargest(arr,length));
+
     return 0;
 }
