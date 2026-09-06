2ef601c4b404658e1d222d58bf7203b4e2883214
Popoye-75 <kasifmohd818@gmail.com>
2026-09-07 00:49:58 +0530

    Changes from linx

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -1,52 +1,146 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int data;
-    struct Node * next;
-    struct Node * prev;
+    struct Node *next;
+    struct Node *prev;
 };
 
-struct Node * head = NULL;  
+struct Node *head = NULL;
 
-void addFirst(int data){
-    struct Node * newNode = (struct Node*)malloc(sizeof(struct Node));
+void addFirst(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = head;
+    if (head != NULL)
+    {
+        head->prev = newNode;
+    }
+    newNode->prev = NULL;
+    head = newNode;
+}
+
+void addLast(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode -> data = data;
-    
-}
+    newNode -> next = NULL;
+    newNode -> prev = NULL;
 
-void addLast(int data){
-    struct Node * newNode = (struct Node *)malloc(sizeof(struct Node));
-    newNode -> data = data;
-}
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
 
-void deleteFirst(){
-    struct Node * temp = head;
-}
-
-void deleteLast(){
-    struct Node * temp = head;
-}
-
-void addAtIndex(int data,int idx){
     struct Node * currNode = head;
+    while(currNode -> next != NULL){
+        currNode = currNode -> next;
+    }
+    currNode -> next = newNode;
+    newNode -> prev = currNode;
 }
 
-void printList(){
-    struct Node * currNode = head;
+void deleteFirst()
+{
+    if (head == NULL)
+    {
+        printf("List is empty .. \n");
+        return;
+    }
+    struct Node *temp = head;
+    head = head->next;
+    if (head != NULL)
+    {
+        head->prev = NULL;
+    }
+    printf("Deleted Data : %d \n", temp->data);
+    free(temp);
 }
 
-int count(){
+void deleteLast()
+{
+    if(head == NULL){
+        printf("List is empty .. \n");
+        return ;
+    }
+    if(head -> next == NULL){
+        struct Node * temp = head;
+        head = NULL;
+        printf("Deleted Data : %d \n",temp->data);
+        free(temp);
+        return;
+    }
+    struct Node *currNode = head;
+    while(currNode -> next != NULL){
+        currNode = currNode -> next;
+    }
+    currNode -> prev -> next = NULL;
+    printf("Deleted Data : %d \n",currNode -> data);
+    free(currNode);
+}
+
+void addAtIndex(int data, int idx)
+{
+    struct Node *currNode = head;
+}
+
+void printList()
+{
+    if(head == NULL){
+        printf("NULL \n");
+        return;
+    }
+    struct Node *currNode = head;
+    while(currNode != NULL){
+        if(currNode -> next != NULL){
+            printf("%d <--> ",currNode -> data);
+        }else{
+            printf("%d --> ",currNode -> data);
+        }
+        currNode = currNode -> next;
+    }
+    printf("NULL \n");
+}
+
+int count()
+{
+    if(head == NULL){
+        return 0;
+    }
     int count = 0;
-
+    struct Node * currNode = head;
+    while(currNode != NULL){
+        count++;
+        currNode = currNode -> next;
+    }
     return count;
 }
 
-void search(){
-    struct Node * currNode = head;
-
+void search()
+{
+    struct Node *currNode = head;
 }
 
-int main(){
+int main()
+{
+    addFirst(5);
+    addFirst(4);
+    addFirst(3);
+    addFirst(2);
+    addFirst(1);
+    printList();
+    printf("Count : %d \n",count());
+    deleteFirst();
+    printf("Count : %d \n",count());
+    printList();
+    printf("Count : %d \n",count());
+    deleteLast();
+    printf("Count : %d \n",count());
+    printList();
+    printf("Count : %d \n",count());
+
     return 0;
 }
